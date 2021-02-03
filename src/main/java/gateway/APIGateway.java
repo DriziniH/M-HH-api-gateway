f008719c9c257c18b400076ab6dc965e5c104abb
Hendrik Höing <hendrikhoing@gmail.com>
2021-02-03 13:13:20 +0100

    Move code to one class

--- src/main/java/gateway/APIGateway.java
+++ src/main/java/gateway/APIGateway.java
@@ -3,6 +3,9 @@ package gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +13,14 @@ public class APIGateway {
 
 	public static void main(String[] args) {
 		SpringApplication.run(APIGateway.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+			return builder.routes()
+					.route(r -> r.path("/eu/**").uri("lb://EU-SERVICE"))
+					.route(r -> r.path("/usa/**").uri("lb://USA-SERVICE"))
+					.build();
 	}
 
 }
